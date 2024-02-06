4639d4ee43d3913d9a868dff99837bca0ec9525e
arjunvm1 <148052201+arjunvm1@users.noreply.github.com>
2024-02-06 14:46:47 +0530

    media app backend

---
+++ index.js
@@ -0,0 +1,30 @@
+//import json server in index.js
+
+const jsonServer = require('json-server')
+
+// create a server for json file
+
+const mediaPlayerServer = jsonServer.create()
+
+//set up a path for json file
+
+const router = jsonServer.router('db.json')
+
+//return a middleware used by json file
+
+const middlewares = jsonServer.defaults()
+
+//set up  a port for runnung json server
+
+const port = 4000 || process.env.port
+
+//use middlewares and router in server
+
+mediaPlayerServer.use(middlewares)
+mediaPlayerServer.use(router)
+
+// to listen server for resolving request from server
+
+mediaPlayerServer.listen(port,()=>{
+    console.log(`mediaplayer is listening at ${port} and waiting for client request !!!`);
+})
